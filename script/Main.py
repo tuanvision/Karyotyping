9ffe167f6559fc1e0f21566741e6e098a4d988fb
lntk <lengoctuankhang@gmail.com>
2018-10-28 22:27:07 +0700

    Conduct some experiments on thresholding
    [PROBLEM] Some bad contours on chromosomes
    [CAUSE] (Maybe) current thresholding method
    [MEASURE] Try different thresholding methods

--- script/Main.py
+++ script/Main.py
@@ -1,14 +1,31 @@
 import cv2
-from preprocessing import background_removal, show_image, sharpen_edges
+from preprocessing import background_removal, show_image, unsharp_masking, opening_by_reconstruction, grisan_local_threshold, adaptive_otsu_threshold
 from os.path import dirname, abspath
 import numpy as np
 from matplotlib import pyplot as plt
 
 image_dir = dirname(dirname(abspath("X"))) + "/data/"
-img = cv2.imread(image_dir + "dapi_raw.BMP")
-filtered_img = cv2.imread(image_dir + "dapi_median_filtered.BMP")
-contours = background_removal(filtered_img)
-image_with_contours = cv2.drawContours(img, contours, contourIdx=-1, color=(0, 0, 255), thickness=1)
-show_image(image_with_contours)
+img = cv2.imread(image_dir + "giemsa_raw.BMP")
+# unsharp_img = unsharp_masking(filtered_img)
+# show_image(img)
+# show_image(filtered_img)
+# show_image(unsharp_img)
+
+# # unsharp_img = unsharp_masking(img)
+# # show_image(img)
+# # show_image(unsharp_img)
+
+# contours = background_removal(img)
+# image_with_contours = cv2.drawContours(img, contours, contourIdx=-1, color=(0, 0, 255), thickness=1)
+# show_image(image_with_contours)
+
+show_image(img)
+show_image(adaptive_otsu_threshold(img))
+
+# img = cv2.imread(image_dir + "dapi_raw.BMP", 0)
+# Iobr = opening_by_reconstruction(img)
+# cv2.imwrite(image_dir + "dapi_result_2610_1010.BMP", Iobr)
+# show_image(Iobr)
+
 
 # cv2.imwrite(image_dir + "dapi_result_2610.BMP", image_with_contours)
--- script/preprocessing.py
+++ script/preprocessing.py
@@ -1,12 +1,27 @@
 import numpy as np
 import cv2
+from skimage.morphology import reconstruction, remove_small_objects, remove_small_holes
 
 def background_removal(image, stride=20, window_size=(100, 100), num_contour_threshold=3, acceptance_ratio=2):
     # convert image to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    show_image(gray, name='gray')
+
+    # use median filter to remove noise
+    gray = cv2.medianBlur(gray, 5)
+
+    # # normalize histogram
+    # gray = histogram_equalization(gray, type="clahe")
+    # show_image(gray, name="he")
+
+    # # perform unsharp masking
+    # gray = unsharp_masking(gray)
+    # show_image(gray, name="unsharp")
 
     # threshold by otsu's method
     ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    # thresh = cv2.adaptiveThreshold(gray, maxValue=255, adaptiveMethod=cv2.ADAPTIVE_THRESH_GAUSSIAN_C, thresholdType=cv2.THRESH_BINARY, blockSize=11, C=2)
+    show_image(thresh, name="thresh")
 
     # get contours
     img2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -56,7 +71,7 @@ def background_removal(image, stride=20, window_size=(100, 100), num_contour_thr
     # draw removed contours
     white_image = 255 - np.zeros_like(gray)
     image_with_chosen_contours = cv2.drawContours(white_image, chosen_contours, contourIdx=-1, color=2, thickness=1)
-    # show_image(image_with_chosen_contours)
+    show_image(image_with_chosen_contours, name="contours")
     return chosen_contours
 
 
@@ -72,12 +87,18 @@ def create_mask_contour(contours, shape):
     return mask_contour
 
 
-def sharpen_edges(image, kernel_size=(5, 5)):
-    kernel_sharpening = np.array([[-1,-1,-1], 
-                                  [-1, 9,-1],
-                                  [-1,-1,-1]])
-    sharpened = cv2.filter2D(image, -1, kernel_sharpening)
-    return sharpened
+def unsharp_masking(image, radius=5, mask_weight=10):
+    gaussian = cv2.GaussianBlur(image, (radius, radius), sigmaX=1)
+    unsharp_image = cv2.addWeighted(image, 1 + mask_weight, gaussian, - mask_weight, gamma=0)
+    return unsharp_image
+
+
+# def sharpen_edges(image, kernel_size=(5, 5)):
+#     kernel_sharpening = np.array([[-1,-1,-1], 
+#                                   [-1, 9,-1],
+#                                   [-1,-1,-1]])
+#     sharpened = cv2.filter2D(image, -1, kernel_sharpening)
+#     return sharpened
 
 
 def show_image(image, name='image'):
@@ -123,3 +144,63 @@ def watershed(image, contours):
     image[markers == -1] = [255,0,0]
     show_image(image)
     return image
+
+# Opening-by-reconstruction = Erosion + Morphological reconstruction
+def opening_by_reconstruction(image):
+    # Erosion
+    se = cv2.getStructuringElement(cv2.MORPH_ERODE, (20, 20)) # structure element
+    Ie = cv2.erode(image, se, iterations=1)
+    show_image(Ie)
+
+    # Morphological reconstruction = iteratively dilation
+    Iobr = reconstruction(Ie, image, method='dilation')
+    return Iobr
+
+
+def histogram_equalization(image, type="clahe"):
+    if type == "normal":
+        result = cv2.equalizeHist(image)
+        return result
+    if type == "clahe":
+        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+        result = clahe.apply(image)
+        return result
+
+    return None
+
+# reference:  Automatic segmentation and disentangling of chromosomes in Q-band prometaphase images
+def grisan_local_threshold(image, grid_size=350):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    height, width = gray.shape
+    matrix_height = int(height/grid_size)
+    matrix_width = int(width/grid_size)
+    matrix_threshold = [[get_grid_threshold_value(gray, i, j, grid_size) for j in range(matrix_width)] for i in range(matrix_height)]
+    matrix_threshold = np.asarray(matrix_threshold)
+    gray_threshold = cv2.resize(matrix_threshold, (width, height), interpolation=cv2.INTER_LINEAR)
+    threshold = np.zeros_like(gray)
+    threshold[gray > gray_threshold] = 255
+    threshold[gray <= gray_threshold] = 0
+    return threshold
+
+# reference: https://dsp.stackexchange.com/questions/2411/what-are-the-most-common-algorithms-for-adaptive-thresholding
+def adaptive_otsu_threshold(image, grid_size=10):
+    
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    threshold = np.zeros_like(gray)
+    height, width = gray.shape    
+    h_upper_bound = int(height/grid_size)
+    w_upper_bound = int(width/grid_size)
+    for i in range(h_upper_bound):
+        for j in range(w_upper_bound):
+            threshold[max(0, i - 1) * grid_size : i * grid_size][max(0, j - 1) * grid_size : j * grid_size] = get_grid_threshold(gray, i, j, grid_size)
+    return threshold
+
+def get_grid_threshold_value(gray, i, j, grid_size):
+    sub_gray = gray[max(0, i - 1) * grid_size : i * grid_size][max(0, j - 1) * grid_size : j * grid_size]
+    ret, thresh = cv2.threshold(sub_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    return ret
+
+def get_grid_threshold(gray, i, j, grid_size):
+    sub_gray = gray[max(0, i - 1) * grid_size : i * grid_size][max(0, j - 1) * grid_size : j * grid_size]
+    ret, thresh = cv2.threshold(sub_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    return thresh
